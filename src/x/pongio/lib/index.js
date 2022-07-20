3932497b80fafc143930a434e8dc0c543982e51b
Tony Pujals <tonypujals@google.com>
2022-07-20 01:36:44 -0700

    pongio protos

--- src/x/pongio/lib/index.js
+++ src/x/pongio/lib/index.js
@@ -8,11 +8,57 @@ const __dirname = path.dirname(__filename);
 const {err, root} = await protobuf.load(path.join(__dirname, "pong.proto"));
 if (err) throw err;
 
+const Timestamp = root.lookupType("pong.Timestamp");
+const EchoRequest = root.lookupType("pong.EchoRequest");
+const EchoReply = root.lookupType("pong.EchoReply");
 const MoveBall = root.lookupType("pong.MoveBall");
 const MovePaddleRequest = root.lookupType("pong.MovePaddleRequest");
 const MovePaddle = root.lookupType("pong.MovePaddle");
 
 export class PongMessages {
+
+  // Encode a message to an Uint8Array (browser) or Buffer (node)
+  encodeEchoRequest(data) {
+    let payload = data;
+    if (process.env.NODE_ENV === "dev") {
+      // Verify the payload if necessary (i.e. when possibly incomplete or invalid)
+      const errMsg = EchoRequest.verify(payload);
+      if (errMsg) throw Error(errMsg);
+    }
+    const message = EchoRequest.create(payload);
+    return EchoRequest.encode(message).finish();
+  }
+
+  // Decode an Uint8Array (browser) or Buffer (node) to an object
+  // returns { x: number, y: number }
+  decodeEchoRequest(buffer) {
+    const message = EchoRequest.decode(buffer);
+    return EchoRequest.toObject(message, {
+      longs: String, enums: String, bytes: String,
+    });
+  }
+
+  // Encode a message to an Uint8Array (browser) or Buffer (node)
+  encodeEchoReply(data) {
+    let payload = data;
+    if (process.env.NODE_ENV === "dev") {
+      // Verify the payload if necessary (i.e. when possibly incomplete or invalid)
+      const errMsg = EchoReply.verify(payload);
+      if (errMsg) throw Error(errMsg);
+    }
+    const message = EchoReply.create(payload);
+    return EchoReply.encode(message).finish();
+  }
+
+  // Decode an Uint8Array (browser) or Buffer (node) to an object
+  // returns { x: number, y: number }
+  decodeEchoReply(buffer) {
+    const message = EchoReply.decode(buffer);
+    return EchoReply.toObject(message, {
+      longs: String, enums: String, bytes: String,
+    });
+  }
+
   // Encode a message to an Uint8Array (browser) or Buffer (node)
   // x, y: number (coordinates) | object { x: number, y: number }
   encodeMoveBall(x, y) {
