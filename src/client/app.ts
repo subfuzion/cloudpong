50509db7c130652edb7c61ae13d9008f22a69ee4
Tony Pujals <tonypujals@google.com>
2022-08-08 15:20:41 -0700

    minor edit

--- src/client/app.ts
+++ src/client/app.ts
@@ -140,16 +140,15 @@ async function main(): Promise<void> {
   const protocol = location.protocol === "https:" ? "wss:" : "ws:";
 
   // Can serve frontend and backend from different hosts:
-  // - during development (using the webpack devServer for frontend).
+  // - during development (using webpack devServer for frontend).
   // - in production (for example, to serve frontend from CDN).
   // Define PONGHOST using DefinePlugin in webpack.config.js. If it is not set,
   // then assume that the host is where the frontend was served from.
   // @ts-ignore (PONGHOST is declared externally in webpack config)
   const host = PONGHOST ? PONGHOST : `${protocol}//${location.host}`;
 
-  // Can have multiple websocket servers.
-  // The client uses the websocket for the first host it can connect to.
-  // For example:
+  // Can have multiple websocket servers. The client uses the websocket for the
+  // first host it can connect to. For example:
   // const hosts = [host, `${protocol}//${location.hostname}:8081`];
   const hosts = [host];
 
