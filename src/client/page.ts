ba717cf1cbf10645c3e27636e2fdd0cafab4259e
Tony Pujals <tonypujals@google.com>
2022-08-09 17:27:32 -0700

    refactor

--- src/client/page.ts
+++ src/client/page.ts
@@ -18,22 +18,29 @@ export class Page {
     // DOM elements (updated by StatsUpdate messages).
     // This is just a temporary example; the stats will be different and
     // rendered as part of the game UI.
-    this.user = document.getElementById("user")!;
-    this.system = document.getElementById("system")!;
-    this.id = document.getElementById("id")!;
-    this.rss = document.getElementById("rss")!;
-    this.heapTotal = document.getElementById("heapTotal")!;
-    this.heapUsed = document.getElementById("heapUsed")!;
-    this.external = document.getElementById("external")!;
+    const id = (id: string) => this.byId(id);
+    this.user = id("user")!;
+    this.system = id("system")!;
+    this.id = id("id")!;
+    this.rss = id("rss")!;
+    this.heapTotal = id("heapTotal")!;
+    this.heapUsed = id("heapUsed")!;
+    this.external = id("external")!;
   }
 
   setStats(m: StatsUpdate): void {
-    this.user!.textContent = m.stats.cpu.user;
-    this.system!.textContent = m.stats.cpu.system;
-    this.id!.textContent = m.id;
-    this.rss!.textContent = m.stats.memory.rss;
-    this.heapTotal!.textContent = m.stats.memory.heapTotal;
-    this.heapUsed!.textContent = m.stats.memory.heapUsed;
-    this.external!.textContent = m.stats.memory.external;
+    this.user.textContent = m.stats.cpu.user;
+    this.system.textContent = m.stats.cpu.system;
+    this.id.textContent = m.id;
+    this.rss.textContent = m.stats.memory.rss;
+    this.heapTotal.textContent = m.stats.memory.heapTotal;
+    this.heapUsed.textContent = m.stats.memory.heapUsed;
+    this.external.textContent = m.stats.memory.external;
+  }
+
+  private byId(id: string): HTMLElement {
+    const e = document.getElementById(id);
+    if (!e) throw new Error(`${id} not found in document`);
+    return e;
   }
 }
