bc6428bfa9a7278cf42a0566de4ff43fc017156c
Tony Pujals <tonypujals@google.com>
2022-08-09 19:28:07 -0700

    refactor

--- src/client/page.ts
+++ src/client/page.ts
@@ -5,28 +5,13 @@ import {StatsUpdate} from "../common/pong/messages";
  * Page handles the UI for everything that is not part of the Pong element.
  */
 export class Page {
-  // stats dom elements
-  user: HTMLElement;
-  system: HTMLElement;
-  id: HTMLElement;
-  rss: HTMLElement;
-  heapTotal: HTMLElement;
-  heapUsed: HTMLElement;
-  external: HTMLElement;
-
-  constructor() {
-    // DOM elements (updated by StatsUpdate messages).
-    // This is just a temporary example; the stats will be different and
-    // rendered as part of the game UI.
-    const id = (id: string) => this.byId(id);
-    this.user = id("user")!;
-    this.system = id("system")!;
-    this.id = id("id")!;
-    this.rss = id("rss")!;
-    this.heapTotal = id("heapTotal")!;
-    this.heapUsed = id("heapUsed")!;
-    this.external = id("external")!;
-  }
+  user = document.getElementById("user")!;
+  system = document.getElementById("system")!;
+  id = document.getElementById("id")!;
+  rss = document.getElementById("rss")!;
+  heapTotal = document.getElementById("heapTotal")!;
+  heapUsed = document.getElementById("heapUsed")!;
+  external = document.getElementById("external")!;
 
   setStats(m: StatsUpdate): void {
     this.user.textContent = m.stats.cpu.user;
@@ -36,11 +21,5 @@ export class Page {
     this.heapTotal.textContent = m.stats.memory.heapTotal;
     this.heapUsed.textContent = m.stats.memory.heapUsed;
     this.external.textContent = m.stats.memory.external;
-  }
-
-  private byId(id: string): HTMLElement {
-    const e = document.getElementById(id);
-    if (!e) throw new Error(`${id} not found in document`);
-    return e;
   }
 }
